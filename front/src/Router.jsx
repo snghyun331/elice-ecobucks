6e4f903f09a1065c82b4caeaf083e253a7e93c33
wonsuk jeong <ssukgoon@gmail.com>
2023-06-02 11:33:47 +0900

    Feat: 블로그페이지, 라우터 추가

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Mall from "./pages/Mall/Mall";
+import Blog from "./pages/Blog/Blog";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/mall" element={<Mall />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
--- front/src/pages/HomePage/HomePage.jsx
+++ front/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <>
       <h3>안녕하세요. 메인페이지입니다.</h3>
+      <Link to="/mall">쇼핑몰로 이동</Link>
+      <br />
+      <Link to="/blog">블로그로 이동</Link>
     </>
   );
 };
