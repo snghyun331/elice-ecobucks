8f811813d7ce5018bb9d016f2ad51d4f2809e92d
wonsuk jeong <ssukgoon@gmail.com>
2023-06-02 15:03:59 +0900

    Feat: 상태가 많이 고장난 헤더 구현

---
+++ front/src/pages/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import LeftNav from "./LeftNav";
+
+const Header = () => {
+  return (
+    <>
+      <header
+        style={{
+          width: "100%",
+          height: "60px",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          top: "0",
+        }}
+      >
+        얍!
+      </header>
+    </>
+  );
+};
+export default Header;
