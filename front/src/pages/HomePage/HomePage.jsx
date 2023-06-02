8deecbb3735bf75718019a8de060a7603c2b07a2
wonsuk jeong <ssukgoon@gmail.com>
2023-06-02 11:08:57 +0900

    feat: 홈페이지, 쇼핑몰, 레이아웃 초안

---
+++ front/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+  return (
+    <>
+      <h3>안녕하세요. 메인페이지입니다.</h3>
+    </>
+  );
+};
+export default HomePage;
---
+++ front/src/pages/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import Router from "../../Router";
+const Layout = () => {
+  return (
+    <>
+      <Router />
+    </>
+  );
+};
+export default Layout;
