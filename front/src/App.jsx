66b335621e91ff352c6a997603e5c6b2b482c753
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-06-02 11:15:24 +0900

    etc : 프리티어 설정 및 기타

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterForm from "./components/Register/RegisterForm";
+
 import './App.css'
 
 function App() {
@@ -10,7 +13,7 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLog`§o} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -28,6 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Router>
+          <Routes>
+            <Route path="/register" element={<RegisterForm />} />
+          </Routes>
+        </Router>
     </>
   )
 }
