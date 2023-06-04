4e8dec55d04128b2ee3d98f31ffb0cea68005600
wonsuk jeong <ssukgoon@gmail.com>
2023-06-04 21:54:39 +0900

    feat: 헤더 구현

--- front/src/pages/Layout/Header.jsx
+++ front/src/pages/Layout/Header.jsx
@@ -1,20 +1,58 @@
-import LeftNav from "./LeftNav";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, BrowserRouter } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <header
-        style={{
-          width: "100%",
-          height: "60px",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          top: "0",
-          position: "fixed",
-        }}
-      >
-        얍!
-      </header>
-    </>
+    <nav
+      className="navbar navbar-expand-sm bg-dark navbar-dark"
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100%",
+      }}
+    >
+      <div className="container-fluid">
+        <a className="nav-brand" href="/">
+          Logo
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="collapsibleNavbar"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Resiter
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Header;
