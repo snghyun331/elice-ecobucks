0d9523ba33cf7e8ade128b54c119b577fd1d8189
wonsuk jeong <ssukgoon@gmail.com>
2023-06-04 20:09:18 +0900

    Feat: initial draft of shopping mall

--- front/src/pages/Mall/Mall.jsx
+++ front/src/pages/Mall/Mall.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+/** 작성자: 정원석
+ * mapping으로 판매할 물건 자동 추가, 삭제
+ */
 import { Container, Button, Card, Row, Col } from "react-bootstrap";
 import Logo from "../../assets/logo.png";
+import SeoulMap from "../../../../data/seoul_map/seoulMap.png"; // 나중에 삭제하기
 
 const Mall = () => {
   return (
@@ -8,6 +11,33 @@ const Mall = () => {
       <Container className="text-center">
         <img src={Logo} className="w-50 mt-5 mb-5" alt="Logo" />
       </Container>
+      <Container
+        style={{
+          width: "60%",
+          height: "30vh",
+          backgroundColor: "#fff",
+          border: "1px solid #000",
+          borderRadius: "10px",
+          padding: "10px",
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        <span>아마 지도 API로 지도가 들어갈 자리</span>
+        <br />
+        <img src={SeoulMap} style={{ maxWidth: "80%", maxHeight: "80%" }} />
+        <Button
+          style={{
+            position: "fixed",
+            right: "10%",
+            marginRight: "20px",
+            marginTop: "20px",
+          }}
+        >
+          떠리상품 판매하기(사장님이)
+        </Button>
+      </Container>
+
       <Container>
         <Row>
           <Col>
