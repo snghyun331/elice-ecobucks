bd6109f692521c89963f3f6324c3007cc6127ade
wonsuk jeong <ssukgoon@gmail.com>
2023-06-04 22:56:52 +0900

    Feat: initial draft of Belog

--- front/src/pages/Blog/Blog.jsx
+++ front/src/pages/Blog/Blog.jsx
@@ -2,6 +2,30 @@ const Blog = () => {
   return (
     <>
       <h3>블로그페이지.</h3>
+      <div className="card">
+        <h5 className="card-header">전기 아끼기 팁</h5>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div className="card">
+        <h5 className="card-header">에어컨 절전모드</h5>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
   );
 };
