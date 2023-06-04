b8bb9255c2ea318de5c224b8114b5d7fa6d36d00
wonsuk jeong <ssukgoon@gmail.com>
2023-06-04 21:58:30 +0900

    docs: Header.jsx 주석

--- front/src/pages/Layout/Header.jsx
+++ front/src/pages/Layout/Header.jsx
@@ -1,5 +1,8 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, BrowserRouter } from "react-router-dom";
+/** 작성자: 정원석
+ * leftNav, rightNav 나눠서 작업하기
+ * leftNav 로고 넣기
+ * rightNav 로그인 여부에 따라 다르게 구현하기
+ */
 
 const Header = () => {
   return (
