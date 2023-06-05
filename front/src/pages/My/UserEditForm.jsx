df12cb6048da4f9c821d79672865cf15981654bf
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-06-05 21:19:55 +0900

    Feat : 비번 수정란 추가

--- front/src/pages/My/UserEditForm.jsx
+++ front/src/pages/My/UserEditForm.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
-import { Row, Form, Button, Dropdown } from "react-bootstrap";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Row, Form, Button, Dropdown, Container } from "react-bootstrap";
+
+import * as Api from "../../api";
+import { DispatchContext, UserStateContext } from "../../App";
 
 const UserEditForm = () => {
+  const navigate = useNavigate();
+  const dispatch = useContext(DispatchContext);
+  const userState = useContext(UserStateContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [name, setName] = useState("");
-  const [greeting, setGreeting] = useState("");
   const [district, setDistrict] = useState(null);
+
+  const [greeting, setGreeting] = useState("");
 
   const districts = [
     "강남구",
@@ -34,18 +46,47 @@ const UserEditForm = () => {
     "중랑구",
   ];
 
+  const validateEmail = (email) => {
+    return email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const validatePassword = (password) => {
+    return password.match(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,18}$/
+    );
+  };
+
+  const validateName = (name) => {
+    return name.match(/^[a-zA-Z가-힣\s]{2,20}$/);
+  };
+
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = validatePassword(password);
+  const isPasswordSame = password === confirmPassword;
+  const isNameValid = validateName(name);
+  const isDistrictValid = district != null;
+
+  const isFormValid =
+    isEmailValid &&
+    isPasswordValid &&
+    isPasswordSame &&
+    isNameValid &&
+    isDistrictValid;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // 여기에서 변경된 정보를 처리하는 로직을 구현합니다.
     // 예를 들어, 서버로 변경된 정보를 전송하거나 상태를 업데이트하는 등의 작업을 수행할 수 있습니다.
     console.log("Name:", name);
     console.log("Email:", email);
+    console.log("Password:", password);
     console.log("Greeting:", greeting);
     console.log("District:", district);
   };
-
-  const isNameValid = name.length >= 2;
-  const isFormValid = isNameValid;
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -59,20 +100,25 @@ const UserEditForm = () => {
         <Form.Control type="file" style={{ borderRadius: "0px" }} />
       </Form.Group>
 
-      <Form.Group controlId="formName">
+      <Form.Group controlId="registerName" className="mt-4">
         <Form.Label
-          className="d-block mt-4"
+          className="text-right d-block"
           style={{ fontWeight: "bold", textAlign: "left" }}
         >
           이름
         </Form.Label>
         <Form.Control
           type="text"
+          autoComplete="off"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          placeholder="이름을 입력하세요"
           style={{ borderRadius: "0px" }}
         />
+        {!isNameValid && name.length > 0 && (
+          <Form.Text className="text-success">
+            이름은 한글과 알파벳만 사용 가능합니다.
+          </Form.Text>
+        )}
       </Form.Group>
 
       <Form.Group controlId="formGreeting">
@@ -92,11 +138,55 @@ const UserEditForm = () => {
         />
       </Form.Group>
 
+      <Form.Group controlId="registerPassword" className="mt-4">
+        <Form.Label
+          className="text-right d-block"
+          style={{ fontWeight: "bold", textAlign: "left" }}
+        >
+          비밀번호 수정
+        </Form.Label>
+        <Form.Control
+          type="password"
+          autoComplete="off"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ borderRadius: "0px" }}
+        />
+          {!isPasswordValid && password.length > 0 && (
+            <Form.Text className="text-success d-block" style={{ textAlign: "left" }}>
+              비밀번호는 알파벳, 숫자, 특수문자를 모두 포함하는 6-18자리어야
+              합니다.
+            </Form.Text>
+          )}
+      </Form.Group>
+
+      <Form.Group controlId="registerConfirmPassword" className="mt-4">
+        <Form.Label
+          className="text-right d-block"
+          style={{ fontWeight: "bold", textAlign: "left" }}
+        >
+          비밀번호 수정 확인
+        </Form.Label>
+        <Form.Control
+          type="password"
+          autoComplete="off"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          style={{ borderRadius: "0px" }}
+        />
+        {!isPasswordSame && (
+            <Form.Text className="text-success d-block" style={{ textAlign: "left" }}>
+            비밀번호가 일치하지 않습니다.
+          </Form.Text>
+        )}
+      </Form.Group>
+
       <Form.Group controlId="registerDistrict" className="mt-4">
-      <Form.Label
+        <Form.Label
           className="d-block mt-4"
           style={{ fontWeight: "bold", textAlign: "left" }}
-        >거주하시는 구
+        >
+          거주하시는 구
           <Row className="text-secondary ms-1" style={{ fontSize: "13px" }}>
             현재 서울시만 서비스하고 있습니다.
           </Row>
