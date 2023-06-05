c16818a3826703384998626322e077104dd7af6e
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-06-05 23:34:31 +0900

    Feat : UserProvider를 분리하여 로그인 동작하고 userState 전역으로 유지하도록 수정

---
+++ front/src/pages/Layout/LeftNav.jsx
@@ -0,0 +1,12 @@
+// import { Link } from "react-router-dom";
+// import Logo from "../../assets/logo.png";
+import { Navbar } from "react-bootstrap";
+const LeftNav = () => {
+  return (
+    <Navbar.Brand href="/" className="navbar-brand">
+      LOGO
+    </Navbar.Brand>
+  );
+};
+
+export default LeftNav;
---
+++ front/src/pages/Layout/RightNav.jsx
@@ -0,0 +1,76 @@
+import { Navbar } from "react-bootstrap";
+
+const RightNav = ({ isLogin }) => {
+  // 로그인 여부에 따라 다른 내용을 렌더링
+  const renderNavContent = () => {
+    if (isLogin) {
+      return (
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/blog">
+              블로그
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/mall">
+              쇼핑몰
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/Logout">
+              로그아웃
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/blog">
+              블로그
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/mall">
+              쇼핑몰
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/register">
+              회원가입
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/login">
+              로그인
+            </a>
+          </li>
+        </ul>
+      );
+    }
+  };
+
+  return (
+    <Navbar
+      className="bg-light"
+      style={{
+        position: "fixed",
+        top: "0",
+        right: "0",
+        width: "100%",
+        backgroundColor: "dark",
+      }}
+    >
+      <div className="container-fluid">
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="collapsibleNavbar"
+        >
+          {renderNavContent()}
+        </div>
+      </div>
+    </Navbar>
+  );
+};
+export default RightNav;
