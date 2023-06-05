83d3aafb54eb1ba307e09f00eab401bcefd69d41
wonsuk jeong <ssukgoon@gmail.com>
2023-06-06 08:51:52 +0900

    CSS: 홈페이지 서울시 전체 전력지도 삽입

--- front/src/pages/HomePage/HomePage.jsx
+++ front/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import { Container, Button} from "react-bootstrap";
+import map from "../../../../data/seoul_map/seoulMap.png";
 import mall from "../../assets/mall.png";
 import tip from "../../assets/tip.png";
 import challenge from "../../assets/challenge.png";
 import TipCarousel from "./TipCarousel";
+import { useState } from "react";
 const HomePage = () => {
+  const [currentImg, setCurrentImg] = useState(map);
+  const handleButtonClick = (img) => {
+    setCurrentImg(img);
+  };
 
   return (
     <div style={{ justifyContent: "center", padding: "60px" }}>
@@ -14,7 +20,7 @@ const HomePage = () => {
             top: 0,
             left: 0,
             right: 0,
-            bottom: "70%",
+            bottom: "60%",
             background: "#4d9e81",
             zIndex: -1
           }}
@@ -76,29 +82,31 @@ const HomePage = () => {
           </Button>
         </Link>
       </Container>
-
+      <span>전력통계</span>
       <Container
         style={{
           width: "100%",
-          height: "30vh",
+          height: "50vh",
           backgroundColor: "#fff",
           border: "1px solid #000",
           borderRadius: "10px",
           padding: "10px",
           marginTop: "10px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <span>전력통계</span>
-        <br />
-        <span>이미지로 지도를 넣거나</span>
-        <br />
-        <span>버튼 형식으로 구 별로 데이터 가져오거나</span>
+        <img src={currentImg}  style={{width:"50%", height:"100%" }} />
         <div
           className="btn-toolbar"
           role="toolbar"
           aria-label="Toolbar with button groups"
         >
           <div className="btn-group me-2" role="group" aria-label="First group">
+            <button type="button" className="btn btn-primary" onClick={()=>handleButtonClick(map)}>
+              서울시
+            </button>
             <button type="button" className="btn btn-primary">
               강남구
             </button>
