3bef95e33391a0cca75743f3781149d270a5a212
wonsuk jeong <ssukgoon@gmail.com>
2023-06-06 19:20:11 +0900

    Feat: 블로그 작성 페이지 라우터 연결

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Mall from "./pages/Mall/Mall";
 import Blog from "./pages/Blog/Blog";
+import BlogPost from "./pages/Blog/BlogPost";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import MyPage from "./pages/My/MyPage";
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/mall" element={<Mall />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/write" element={<BlogPost />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/my" element={<MyPage />} />
---
+++ front/src/pages/Blog/BlogPost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BlogPost = () => {
+  // 글 작성 페이지의 내용과 기능을 구현합니다.
+
+  return (
+    <div>
+      {/* 글 작성 페이지의 내용을 구현합니다. */}
+      라우터에 연결해야하나...
+    </div>
+  );
+};
+
+export default BlogPost;
