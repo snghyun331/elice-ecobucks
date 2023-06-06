bb8ab3b09cd31b8f4fc1ee2efd54637f4c52827e
wonsuk jeong <ssukgoon@gmail.com>
2023-06-06 09:12:31 +0900

    CSS: 홈페이지 전력사용량 그래프 삽입

--- front/src/pages/HomePage/HomePage.jsx
+++ front/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { Container, Button} from "react-bootstrap";
 import map from "../../../../data/seoul_map/seoulMap.png";
+import DomesticAverage from "../../../../data/DomesticAverage.png";
+import DomesticDistrict from "../../../../data/DomesticDistrict.png";
+import Industry from "../../../../data/Industry.png";
 import mall from "../../assets/mall.png";
 import tip from "../../assets/tip.png";
 import challenge from "../../assets/challenge.png";
@@ -99,25 +102,22 @@ const HomePage = () => {
       >
         <img src={currentImg}  style={{width:"50%", height:"100%" }} />
         <div
-          className="btn-toolbar"
+          className="btn-toolbar justify-content-md-end"
           role="toolbar"
           aria-label="Toolbar with button groups"
         >
-          <div className="btn-group me-2" role="group" aria-label="First group">
+          <div className="d-grid gap-2 mx-auto justify-content-md-end">
             <button type="button" className="btn btn-primary" onClick={()=>handleButtonClick(map)}>
-              서울시
+              서울시 전체
             </button>
-            <button type="button" className="btn btn-primary">
-              강남구
+            <button type="button" className="btn btn-primary" onClick={()=>handleButtonClick(Industry)}>
+              산업용
             </button>
-            <button type="button" className="btn btn-primary">
-              강동구
+            <button type="button" className="btn btn-primary" onClick={()=>handleButtonClick(DomesticAverage)}>
+              서울시 평균 사용량(가정용)
             </button>
-            <button type="button" className="btn btn-primary">
-              강북구
-            </button>
-            <button type="button" className="btn btn-primary">
-              강서구
+            <button type="button" className="btn btn-primary" onClick={()=>handleButtonClick(DomesticDistrict)}>
+              구 별 사용량(가정용)
             </button>
           </div>
         </div>
