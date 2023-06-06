b2333e81d2f0ba339e3ba0f33d27377f31518dfc
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-06-07 01:39:57 +0900

    Style: 유저 수정 함수,변수 이름 수정

--- back/src/controllers/user-controller.js
+++ back/src/controllers/user-controller.js
@@ -134,14 +134,14 @@ const userDeleteWithdraw = async function (req, res, next) {
 const userPutMypage = async function (req, res, next) {
   try {
     // URI로부터 사용자 id를 추출함.
-    const user_id = req.params._id;
+    const userId = req.params._id;
 
     // body data 로부터 업데이트할 사용자 정보를 추출함.
     const { username, guName } = req.body ?? null;
     const toUpdate = { username, guName };
 
     // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트함. 업데이트 요소가 없을 시 생략함
-    const updatedUser = await userAuthService.setUser({ user_id, toUpdate });
+    const updatedUser = await userAuthService.updateUser({ userId, toUpdate });
 
     if (updatedUser.errorMessage) {
       throw new Error(updatedUser.errorMessage);
