65122587d00953b91f3f5f4e0dd6f3d99a8282f5
wonsuk jeong <ssukgoon@gmail.com>
2023-06-06 20:11:15 +0900

    Feat: 블로그 팁 작성 페이지 초안

--- front/src/pages/Blog/BlogPost.jsx
+++ front/src/pages/Blog/BlogPost.jsx
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const BlogPost = () => {
-  // 글 작성 페이지의 내용과 기능을 구현합니다.
+const BlogPost = (props) => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   return (
-    <div>
-      {/* 글 작성 페이지의 내용을 구현합니다. */}
-      라우터에 연결해야하나...
+    <div style={{ padding: "16px", width: "calc(100% - 32px)" }}>
+      <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: "70%",
+              background: "#4d9e81",
+              zIndex: -1,
+            }}
+          ></div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ width: "100%", maxWidth: "720px", padding:"60px" }}>
+          <textarea
+            style={{
+              width: "100%",
+              height: "20px",
+              padding: "16px",
+              fontSize: "16px",
+              lineHeight: "20px",
+              marginBottom: "16px",
+            }}
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+
+          <textarea
+            style={{
+              width: "100%",
+              height: "480px",
+              padding: "16px",
+              fontSize: "16px",
+              lineHeight: "20px",
+              marginBottom: "16px",
+            }}
+            value={content}
+            onChange={(event) => {
+              setContent(event.target.value);
+            }}
+          />
+
+          <button
+            style={{
+              padding: "8px 16px",
+              fontSize: "16px",
+              borderWidth: "1px",
+              borderRadius: "8px",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            글 작성하기
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
