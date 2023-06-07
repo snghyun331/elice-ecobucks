4b68adf797d8e2e12af4e96887aedda45fb116a0
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-06-07 17:42:18 +0900

    Feat : 챌린지 댓글 기능 추가

--- front/src/pages/Challenge/ChallengeRead.jsx
+++ front/src/pages/Challenge/ChallengeRead.jsx
@@ -1,6 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Container, Card, Modal } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Card,
+  Modal,
+  Form,
+  ListGroup,
+} from "react-bootstrap";
 import * as Api from "../../api";
 
 import ChallengeParticipate from "./ChallengeParticipate";
@@ -11,6 +18,7 @@ import ChallengeUpdate from "./ChallengeUpdate";
 const ChallengeRead = ({ challenge, onBackToListClick }) => {
   const [showModal, setShowModal] = useState(false);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [comments, setComments] = useState([]);
 
   const userState = useContext(UserStateContext);
   const navigate = useNavigate();
@@ -35,7 +43,7 @@ const ChallengeRead = ({ challenge, onBackToListClick }) => {
     const date = new Date(dateString);
     return date.toLocaleDateString(); // Format date as 'YYYY-MM-DD'
   };
-  console.log("리드", userState)
+  console.log("리드", userState);
   const isCurrentUserAuthor = userState.user._id === challenge.userId._id;
 
   const handleDeleteClick = async () => {
@@ -49,7 +57,65 @@ const ChallengeRead = ({ challenge, onBackToListClick }) => {
       }
     }
   };
+
+  const handleAddComment = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const content = formData.get("content");
   
+    try {
+      const response = await Api.post(`challenges/${challenge._id}/comments`, { content });
+      setComments([...comments, response.data]);
+    } catch (error) {
+      console.log("Error adding comment:", error);
+    }
+  
+    event.target.reset();
+  };
+  
+  
+
+  const handleEditComment = async (commentId, content) => {
+    try {
+      await Api.put(`challenges/${challenge._id}/comments/${commentId}`, {
+        content,
+      });
+      const updatedComments = comments.map((comment) => {
+        if (comment._id === commentId) {
+          return { ...comment, content };
+        }
+        return comment;
+      });
+      setComments(updatedComments);
+    } catch (error) {
+      console.log("Error editing comment:", error);
+    }
+  };
+
+  const handleDeleteComment = async (commentId) => {
+    try {
+      await Api.delete(`challenges/${challenge._id}/comments/${commentId}`);
+      const updatedComments = comments.filter(
+        (comment) => comment._id !== commentId
+      );
+      setComments(updatedComments);
+    } catch (error) {
+      console.log("Error deleting comment:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await Api.get(`challenges/${challenge._id}/comments`);
+        setComments(response.data);
+      } catch (error) {
+        console.log("Error fetching comments:", error);
+      }
+    };
+
+    fetchComments();
+  }, [challenge]);
 
   return (
     <Container>
@@ -67,12 +133,56 @@ const ChallengeRead = ({ challenge, onBackToListClick }) => {
             <br />
             참여인원: {challenge.participantsCount.toLocaleString()} 명
           </Card.Text>
+
+          <h4>댓글</h4>
+          <ListGroup>
+            {comments.map((comment) => (
+              <ListGroup.Item key={comment._id}>
+                {comment.content}
+                {comment.userId === userState.user._id && (
+                  <Button
+                    variant="link"
+                    className="btn-sm"
+                    onClick={() =>
+                      handleEditComment(
+                        comment._id,
+                        prompt("Edit comment", comment.content)
+                      )
+                    }
+                  >
+                    수정
+                  </Button>
+                )}
+                {comment.userId === userState.user._id && (
+                  <Button
+                    variant="link"
+                    className="btn-sm text-danger"
+                    onClick={() => handleDeleteComment(comment._id)}
+                  >
+                    삭제
+                  </Button>
+                )}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Form onSubmit={handleAddComment}>
+            <Form.Group controlId="content">
+              <Form.Control
+                name="content"
+                as="textarea"
+                rows={3}
+                placeholder="댓글을 입력하세요..."
+                required
+              />
+            </Form.Group>
+            <Button type="submit">댓글 추가</Button>
+          </Form>
         </Card.Body>
       </Card>
       <>
-      <Button className="mt-3" onClick={handleJoinClick}>
-        참가하기
-      </Button>
+        <Button className="mt-3" onClick={handleJoinClick}>
+          참가하기
+        </Button>
       </>
       {isCurrentUserAuthor && (
         <>
@@ -88,18 +198,17 @@ const ChallengeRead = ({ challenge, onBackToListClick }) => {
         목록으로
       </Button>
 
-
-
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>참가하기</Modal.Title>
         </Modal.Header>
-        <ChallengeParticipate onClose={handleCloseModal} challenge={challenge} />
+        <ChallengeParticipate
+          onClose={handleCloseModal}
+          challenge={challenge}
+        />
       </Modal>
 
-
-
-      <Modal size='lg' show={showUpdateModal} onHide={handleCloseUpdateModal}>
+      <Modal size="lg" show={showUpdateModal} onHide={handleCloseUpdateModal}>
         <Modal.Header closeButton>
           <Modal.Title>챌린지 수정</Modal.Title>
         </Modal.Header>
@@ -108,9 +217,6 @@ const ChallengeRead = ({ challenge, onBackToListClick }) => {
           onClose={handleCloseUpdateModal}
         />
       </Modal>
-
-
-
     </Container>
   );
 };
