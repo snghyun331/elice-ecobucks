f3f87f3f06c01484523f4ed40b9c74aac6bafb5a
wonsuk jeong <ssukgoon@gmail.com>
2023-06-07 13:43:35 +0900

    필요없는 라우터 blogpost  삭제

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Mall from "./pages/Mall/Mall";
 import Blog from "./pages/Blog/Blog";
-import BlogPost from "./pages/Blog/BlogPost";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import MyPage from "./pages/My/MyPage";
@@ -15,7 +14,6 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/mall" element={<Mall />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/write" element={<BlogPost />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/my" element={<MyPage />} />
