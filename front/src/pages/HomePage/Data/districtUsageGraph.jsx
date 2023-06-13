3123cdfa382c0ea83e6444eb3c2f037764832c42
wonsuk jeong <ssukgoon@gmail.com>
2023-06-14 02:55:06 +0900

    Data: 구, 월 평균 데이터 추출, 그래프는 못그림

--- front/src/pages/HomePage/Data/districtUsageGraph.jsx
+++ front/src/pages/HomePage/Data/districtUsageGraph.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ResponsiveBar } from '@nivo/bar';
+import { ResponsiveLine } from '@nivo/line';
 import * as Api from "../../../api";
 
 //구별 월평균 데이터
@@ -16,136 +16,144 @@ const DistrictChart = () => {
         try {
             const response = await Api.get('data/districtUsage');
             // const data = await response.json();
-            console.log('data', response)
-            setChartData(response);
+            // console.log('data: ', response);
+            
+            // 도시 이름으로 나누기
+            const dataByCity = {};
+            response.data.forEach(item => {
+                const cityName = item.city;
+                if (!dataByCity.hasOwnProperty(cityName)) {
+                    dataByCity[cityName] = [];
+                }
+                dataByCity[cityName].push(item);
+            });
+            const cityData = Object.values(dataByCity);
+            // console.log("cityData: ", cityData);
+            setChartData(cityData);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
+    // console.log("chartData: ", chartData);
+    const calculateAvgPowerUsage = () => {
+        const avgPowerUsageByCity = [];
 
+        chartData.forEach(cityData => {
+            // if (!cityData || !cityData.length || !cityData[0].hasOwnProperty('city')) {
+            //     return; // 건너뛰기
+            //   }
+            const { city } = cityData;
+            console.log("차트데이터안 폴이치: ", city);
+            const monthlyAvgs = {};
+
+            cityData.forEach(data => {
+                const { month, powerUsage } = data;
+
+                if (!monthlyAvgs.hasOwnProperty(month)) {
+                    monthlyAvgs[month] = {
+                        totalPowerUsage: 0,
+                        count: 0
+                    };
+                }
+
+                monthlyAvgs[month].totalPowerUsage += powerUsage;
+                monthlyAvgs[month].count++; 
+            });
+
+            const monthlyAvgsArray = Object.entries(monthlyAvgs).map(([month, { totalPowerUsage , count }]) => ({
+                month,
+                avgPowerUsage: totalPowerUsage / count
+            }));
+
+            avgPowerUsageByCity.push({
+                city,
+                monthlyAvgs: monthlyAvgsArray
+            });
+        });
+
+        return avgPowerUsageByCity;
+    }
+    const avgPowerUsageByCity = calculateAvgPowerUsage();
+    // console.log(avgPowerUsageByCity);
+    // console.log(avgPowerUsageByCity[0].monthlyAvgs);
+    // const transformedData = [];
+    // const transformedData = avgPowerUsageByCity.map(item => ({
+    //     x: item.monthlyAvgs[0].month,
+    //     y: item.avgPowerUsage
+    //   }));
+    // console.log("transformedData: ", transformedData);
+    console.log(avgPowerUsageByCity[0]);
+    console.log(avgPowerUsageByCity[0].monthlyAvgs[0].month);
+
+    const transformedData = avgPowerUsageByCity.map(item => {
+        const monthlyAvgs = item.monthlyAvgs.map(monthlyData => ({
+            x: monthlyData.month,
+            y: monthlyData.avgPowerUsage
+        }));
+
+        return {
+            data: monthlyAvgs
+        };
+    });
+    console.log(transformedData);
     return (
-        <div>
-            {/* 이거 날려,,, 이거 무시하세요.. */}
-            <h2>My chart</h2>
-
-            <ResponsiveBar
-                data={chartData}
-                keys={[
-                    'hot dog',
-                    'burger',
-                    'sandwich',
-                    'kebab',
-                    'fries',
-                    'donut'
-                ]}
-                indexBy="country"
-                margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-                padding={0.2}
-                innerPadding={2}
-                groupMode="grouped"
-                valueScale={{ type: 'linear' }}
-                indexScale={{ type: 'band', round: true }}
-                valueFormat=" >-"
-                colors={{ scheme: 'purple_blue' }}
-                defs={[
-                    {
-                        id: 'dots',
-                        type: 'patternDots',
-                        background: 'inherit',
-                        color: '#38bcb2',
-                        size: 4,
-                        padding: 1,
-                        stagger: true
-                    },
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'inherit',
-                        color: '#eed312',
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10
-                    }
-                ]}
-                fill={[
-                    {
-                        match: {
-                            id: 'fries'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'sandwich'
-                        },
-                        id: 'lines'
-                    }
-                ]}
-                borderColor={{
-                    from: 'color',
-                    modifiers: [
-                        [
-                            'darker',
-                            '1.6'
-                        ]
-                    ]
-                }}
+        <div style={{ width: 500, height: 450, marginLeft:"20px" }}>
+            <ResponsiveLine
+                data={[{ data: transformedData }]}
+                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                xScale={{ type: 'point' }}
+                yScale={{ type: 'linear', min: 220, max: 250, stacked: true, reverse: false }}
+                yFormat=" >-.2f"
                 axisTop={null}
                 axisRight={null}
                 axisBottom={{
                     tickSize: 5,
                     tickPadding: 5,
                     tickRotation: 0,
-                    legend: 'country',
-                    legendPosition: 'middle',
-                    legendOffset: 32
+                    legend: 'Month',
+                    legendOffset: 36,
+                    legendPosition: 'middle'
                 }}
                 axisLeft={{
                     tickSize: 5,
                     tickPadding: 5,
                     tickRotation: 0,
-                    legend: 'food',
-                    legendPosition: 'middle',
-                    legendOffset: -40
+                    legend: 'Power Usage',
+                    legendOffset: -40,
+                    legendPosition: 'middle'
                 }}
-                labelSkipWidth={12}
-                labelSkipHeight={12}
-                labelTextColor={{
-                    from: 'color',
-                    modifiers: [
-                        [
-                            'darker',
-                            1.6
-                        ]
-                    ]
-                }}
+                pointSize={10}
+                pointColor={{ theme: 'background' }}
+                pointBorderWidth={2}
+                pointBorderColor={{ from: 'serieColor' }}
+                pointLabelYOffset={-12}
+                useMesh={true}
                 legends={[
                     {
-                        dataFrom: 'keys',
                         anchor: 'bottom-right',
                         direction: 'column',
                         justify: false,
-                        translateX: 120,
+                        translateX: 100,
                         translateY: 0,
-                        itemsSpacing: 2,
-                        itemWidth: 100,
-                        itemHeight: 20,
+                        itemsSpacing: 0,
                         itemDirection: 'left-to-right',
-                        itemOpacity: 0.85,
-                        symbolSize: 20,
+                        itemWidth: 80,
+                        itemHeight: 20,
+                        itemOpacity: 0.75,
+                        symbolSize: 12,
+                        symbolShape: 'circle',
+                        symbolBorderColor: 'rgba(0, 0, 0, .5)',
                         effects: [
                             {
                                 on: 'hover',
                                 style: {
+                                    itemBackground: 'rgba(0, 0, 0, .03)',
                                     itemOpacity: 1
                                 }
                             }
                         ]
                     }
                 ]}
-                role="application"
-                ariaLabel="Nivo bar chart demo"
-                barAriaLabel={e => e.id + ": " + e.formattedValue + " in country: " + e.indexValue}
             />
         </div>
     );
