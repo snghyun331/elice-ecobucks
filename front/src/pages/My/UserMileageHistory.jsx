d3a4878c2629e056ff16edfa64c689e88419c21e
[AI7/2반]정소현 <48352775gh@gmail.com>
2023-06-16 17:13:00 +0900

    Feat : 마이페이지 마일리지 히스토리 연결 완료

--- front/src/pages/My/UserMileageHistory.jsx
+++ front/src/pages/My/UserMileageHistory.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Table, Pagination, Container } from "react-bootstrap";
+import { Table, Container } from "react-bootstrap";
 import * as Api from "../../api";
 import { formatDate } from "../../util/common";
 import PagenationBar from "../Modal/PaginationBar";
 
 const UserMileageHistory = ({ user }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  // const mileagesPerPage = 5;
   const [mileageHistory, setMileageHistory] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
 
@@ -18,7 +17,7 @@ const UserMileageHistory = ({ user }) => {
         if (res.data.message) {
           setMileageHistory([]);
         } else {
-          setMileageHistory(res.data.userChallengeList);
+          setMileageHistory(res.data.newParticipations.userParticipationList || []);
           setTotalPages(res.data.totalPages);
         }
       } catch (err) {
@@ -29,7 +28,6 @@ const UserMileageHistory = ({ user }) => {
     fetchMileageHistory();
   }, [user, currentPage]);
 
-  // 페이지네이션 클릭 시 페이지 변경
   const handlePageChange = (newPage) => {
     if (newPage < 1) {
       setCurrentPage(1);
@@ -48,28 +46,36 @@ const UserMileageHistory = ({ user }) => {
             <th>적립 날짜</th>
             <th>적립 금액</th>
             <th>챌린지/활동</th>
+            <th>테마 아이콘</th>
           </tr>
         </thead>
         <tbody>
-          {mileageHistory.map((mileage, index) => (
-            <tr key={index} style={{ fontSize: "0.8rem" }}>
-              <td style={{ width: "25%" }}>{formatDate(mileage.updatedAt)}</td>
-              <td style={{ width: "25%" }}>1,000</td>
-              <td style={{ width: "50%" }}>{mileage.challengeTitle}</td>
+          {mileageHistory.length > 0 ? (
+            mileageHistory.map((mileage, index) => (
+              <tr key={index} style={{ fontSize: "0.8rem" }}>
+                <td style={{ width: "25%" }}>{formatDate(mileage.participationCreatedAt)}</td>
+                <td style={{ width: "25%" }}>1,000</td>
+                <td style={{ width: "25%" }}>{mileage.challengeTitle}</td>
+                <td style={{ width: "25%" }}>{mileage.challengeIcon}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={3} style={{ textAlign: "center" }}>
+                No mileage history available.
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </Table>
 
-      {
-        <Container className="d-flex justify-content-center">
-          <PagenationBar
-            totalPages={totalPages}
-            handlePageChange={handlePageChange}
-            currentPage={currentPage}
-          />
-        </Container>
-      }
+      <Container className="d-flex justify-content-center">
+        <PagenationBar
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+          currentPage={currentPage}
+        />
+      </Container>
     </Container>
   );
 };
